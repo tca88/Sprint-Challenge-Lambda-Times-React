dd0b82fbf1600986f434f2c7329cd6323a59bdb5
tca88 <trishnaamin@gmail.com>
2019-03-29 12:04:59 -0400

    Install styled components, proptype and reactstrap packages. Go through instructions and complete functions and functional components required to render and filter through tabs on the screen.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
-      tabs: [],
-      cards: []
+      selected: "all",
+      tabs: tabData,
+      cards: cardData
     };
   }
 
-  componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
-  }
+  // componentDidMount() {
+  //   this.setState({
+  //     tabs: tabData,
+  //     cards: cardData
+  //   });
+  // }
 
-  changeSelected = tab => {
+  selectTabHandler = tab => {
     // this function should take in the tab and update the state with the new tab.
+    console.log(tab);
+    this.setState({
+      selected: tab
+    });
   };
 
   filterCards = () => {
@@ -37,7 +46,9 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
+    if (this.state.selected === "all") return cardData;
+    return this.state.cards.filter(card => card.tab === this.state.selected);
   };
 
   render() {
@@ -48,9 +59,17 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectTabHandler={this.selectTabHandler}
+          selectedTab={this.state.selected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
   }
 }
+
+Content.propTypes = {
+  selected: PropTypes.string
+};
